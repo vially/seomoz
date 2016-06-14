3d264cca2a7783f9cd268b251a0e6dbac941a298
Valentin Hăloiu <vially.ichb@gmail.com>
2016-06-14 03:38:13 +0300

    Bulk check URLs in cli app

--- cmd/seomoz/main.go
+++ cmd/seomoz/main.go
@@ -2,32 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/jawher/mow.cli"
 	"github.com/vially/seomoz"
 	"log"
 	"os"
-	"strconv"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: seomoz URL [COLS]")
-		os.Exit(1)
-	}
+	app := cli.App("seomoz", "Analyze URLs using SEOmoz")
 
-	queryURL := os.Args[1]
-	cols := seomoz.DefaultCols
-	if len(os.Args) > 2 {
-		if columns, err := strconv.Atoi(os.Args[2]); err != nil {
-			log.Fatalln("Invalid COLS value: " + os.Args[2])
-		} else {
-			cols = columns
+	app.Spec = "[--cols=<SEOmoz COLS>] URL..."
+
+	var (
+		cols = app.IntOpt("c cols", seomoz.DefaultCols, "SEOmoz COLS")
+		urls = app.StringsArg("URL", nil, "URLs to analyze")
+	)
+
+	app.Action = func() {
+		client := seomoz.NewEnvClient()
+		response, err := client.GetBulkURLMetrics(*urls, *cols)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for _, metrics := range response {
+			fmt.Printf("%s\tLinks: %.0f\tPage Authority: %.0f\tDomain Authority: %.0f\n", metrics.URL, metrics.Links, metrics.PageAuthority, metrics.DomainAuthority)
 		}
 	}
 
-	client := seomoz.NewEnvClient()
-	metrics, err := client.GetURLMetrics(queryURL, cols)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Printf("URL: %s\nLinks: %.0f\nPage Authority: %.0f\nDomain Authority: %.0f\n", metrics.URL, metrics.Links, metrics.PageAuthority, metrics.DomainAuthority)
+	app.Run(os.Args)
 }
